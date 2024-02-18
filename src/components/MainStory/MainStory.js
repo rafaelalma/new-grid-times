817be5654661013636c6ebebe3186eb0bafa37d2
rafaelalma <rafaelalma92@gmail.com>
2024-02-18 20:28:41 +0100

    feat: clamp main story text

--- src/components/MainStory/MainStory.js
+++ src/components/MainStory/MainStory.js
@@ -1,14 +1,9 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-const MainStory = ({
-  id,
-  title,
-  image,
-  location,
-  abstract,
-  ...delegated
-}) => {
+import { QUERIES } from '../../constants'
+
+const MainStory = ({ id, title, image, location, abstract, ...delegated }) => {
   return (
     <Wrapper {...delegated}>
       <a href={`/story/${id}`}>
@@ -20,35 +15,43 @@ const MainStory = ({
       </Abstract>
       <ReadMore href="/story">Continue Reading…</ReadMore>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.article`
   color: var(--color-gray-900);
-`;
+`
 
 const Image = styled.img`
   display: block;
   width: 100%;
   margin-bottom: 12px;
-`;
+`
 
 const Heading = styled.h2`
   margin-bottom: 8px;
   font-size: 1.5rem;
   font-weight: var(--font-weight-bold);
   line-height: 1.3;
-`;
+`
 
 const Abstract = styled.p`
   font-size: 1rem;
   margin-bottom: 1em;
   white-space: pre-wrap;
-`;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 8;
+  overflow: hidden;
+
+  @media ${QUERIES.tabletAndUp} {
+    -webkit-line-clamp: 16;
+  }
+`
 
 const Location = styled.span`
   text-transform: uppercase;
-`;
+`
 
 const ReadMore = styled.a`
   font-weight: var(--font-weight-medium);
@@ -58,6 +61,6 @@ const ReadMore = styled.a`
     text-decoration: underline;
     text-underline-offset: 1px;
   }
-`;
+`
 
-export default MainStory;
+export default MainStory
