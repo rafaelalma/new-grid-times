212c611610fd3484f90aa8d1a2ff561291b8afd2
rafaelalma <rafaelalma92@gmail.com>
2024-02-18 12:27:47 +0100

    feat: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from 'react'
+import styled from 'styled-components/macro'
+import { Menu, Search, User } from 'react-feather'
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from '../../constants'
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from '../MaxWidthWrapper'
+import Logo from '../Logo'
+import Button from '../Button'
 
 const Header = () => {
   return (
-    <header>
+    <Wrapper>
       <SuperHeader>
         <Row>
           <ActionGroup>
@@ -22,29 +22,53 @@ const Header = () => {
             </button>
           </ActionGroup>
           <ActionGroup>
-            <button>
+            <MobileButton>
               <User size={24} />
-            </button>
+            </MobileButton>
           </ActionGroup>
+          <SubscribeWrapper>
+            <Button>Subscribe</Button>
+            <Link>Already a subscriber?</Link>
+          </SubscribeWrapper>
         </Row>
       </SuperHeader>
       <MainHeader>
         <Logo />
       </MainHeader>
-    </header>
-  );
-};
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.header`
+  @media ${QUERIES.laptopAndUp} {
+    position: relative;
+    margin: 16px 0 81px 0;
+  }
+`
 
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
-`;
+
+  @media ${QUERIES.laptopAndUp} {
+    background: var(--color-gray-100);
+    color: var(--color-gray-900);
+    min-height: 98px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`
 
 const Row = styled(MaxWidthWrapper)`
   display: flex;
   justify-content: space-between;
-`;
+
+  @media ${QUERIES.laptopAndUp} {
+    flex: 1;
+  }
+`
 
 const ActionGroup = styled.div`
   display: flex;
@@ -57,7 +81,7 @@ const ActionGroup = styled.div`
   svg {
     display: block;
   }
-`;
+`
 
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
@@ -65,6 +89,34 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
-`;
 
-export default Header;
+  @media ${QUERIES.laptopAndUp} {
+    position: absolute;
+    inset: 0;
+  }
+`
+
+const MobileButton = styled.button`
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`
+
+const SubscribeWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+  }
+`
+
+const Link = styled.a`
+  font-size: 0.875rem;
+  font-style: italic;
+  text-decoration: underline;
+`
+
+export default Header
