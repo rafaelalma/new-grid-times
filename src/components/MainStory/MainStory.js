11800d1ebefef736bf8389277eecec2eb62866f3
rafaelalma <rafaelalma92@gmail.com>
2024-02-18 21:10:58 +0100

    feat: add secondary story text clamping

--- src/components/MainStory/MainStory.js
+++ src/components/MainStory/MainStory.js
@@ -47,6 +47,10 @@ const Abstract = styled.p`
   @media ${QUERIES.tabletAndUp} {
     -webkit-line-clamp: 16;
   }
+
+  @media ${QUERIES.laptopAndUp} {
+    -webkit-line-clamp: 10;
+  }
 `
 
 const Location = styled.span`
